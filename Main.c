1e860cf1367f4d60f0c02b63f8fedc9c1e84352e
andre.freddi <52337680+andrefreddi@users.noreply.github.com>
2020-04-28 20:00:22 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -121,26 +121,26 @@ void forcabruta(char palavras[], char frase[])
 	int tPalavra = strlen(palavras); //Define tamanho da string
     int tFrase = strlen(frase); //Define tamanho da frase que busca
     
-    int cont = 0; //Contador para conclusão
+    int cont = 0; //Contador para conclusÃ£o
     int k = 0; //contador usado na string e na frase
     int j = 0; //Contador usado na frase
     int i = 0; //Contador usado na string
 
-    for (i=0;  i < tPalavra - tFrase; i++) { //Define o tamanho da string qual ainda é valido tentar a comparação, e define primeira letra da string.
+    for (i=0;  i < tPalavra - tFrase; i++) { //Define o tamanho da string qual ainda Ã© valido tentar a comparaÃ§Ã£o, e define primeira letra da string.
         k=i;  cont=1;
         for(j=0 ; j < tFrase; j++) { //Define primeira letra da string
-            if ((palavras[k]==frase[j])  && (cont != tFrase)){ //Compara primeira letra da frase com a posição atual da string
-            cont++;k++; //Se igual soma o contador para conclusão e a posição da string (k)
-            }//Ao retornar para o for soma a posição da frase (j)
-        }//Ao retornar para o primeiro for soma a posição da string (i)
-            if (cont == tFrase){ //Se o tamanho do contador de conclusão somar ate chegar ao tamanho da frase quer dizer que a string foi encontrada.
+            if ((palavras[k]==frase[j])  && (cont != tFrase)){ //Compara primeira letra da frase com a posiÃ§Ã£o atual da string
+            cont++;k++; //Se igual soma o contador para conclusÃ£o e a posiÃ§Ã£o da string (k)
+            }//Ao retornar para o for soma a posiÃ§Ã£o da frase (j)
+        }//Ao retornar para o primeiro for soma a posiÃ§Ã£o da string (i)
+            if (cont == tFrase){ //Se o tamanho do contador de conclusÃ£o somar ate chegar ao tamanho da frase quer dizer que a string foi encontrada.
                 printf(" > A frase mencionada comeca na posicao %d e termina na posicao %d do array.\n > ", i, k); //Imprimi a posicao de inicio e fim
                 for (i; i <= k; i++){ //Imprimir cada letra
                     printf("%c", palavras[i]);
                 }
                 printf("\n\n\n");
                 system("pause");
-                return 0; //Finaliza com o resultado OK
+               
             } 
     }
     printf(" > Frase nao foi encontrada.\n");
@@ -184,7 +184,7 @@ void rabinkarp(char palavras[], char frase[]){
                     break;
             }
             if (tFrase == j){ //Se o tamanho da String (tFrase) for do mesmo tamanho do (j) quer dizer que a string foi encontrada.
-                int k = i + tFrase -1; //Calcula posição final da frase para imprimir na tela
+                int k = i + tFrase -1; //Calcula posiÃ§Ã£o final da frase para imprimir na tela
                 printf(" > A frase mencionada comeca na posicao %d e termina na posicao %d do array.\n > ", i, k); //Imprimi a posicao de inicio e fim
                 for (i; i <= k; i++){ //Imprimir cada letra
                     printf("%c", palavras[i]);
